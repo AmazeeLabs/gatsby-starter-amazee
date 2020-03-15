954fa9faf4e53701dd5873d4abe424be29028667
JohnAlbin <virtually.johnalbin@gmail.com>
2020-03-15 21:05:03 +0800

    Add autoprefixer to postcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   plugins: [
+    // Transform @import rules by inlining content.
+    require('postcss-import'),
     // Automatic prefixing and browser compatibility.
     require('postcss-preset-env')({ stage: 0 }),
-    // Include tailwind base styles.
+    // Apply tailwind features.
     require('tailwindcss')('tailwind.config.js'),
-    // Get rid of comments.
+    // Strip CSS comments.
     require('postcss-discard-comments'),
+    // Add vendor prefixes.
+    require('autoprefixer'),
   ].concat(
     process.env.NODE_ENV === 'production'
       ? [
