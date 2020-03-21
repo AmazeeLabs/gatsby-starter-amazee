0b64e4b9d9c62e24cc75629124c267d643043de2
John Albin Wilkins <virtually.johnalbin@gmail.com>
2020-03-21 17:35:50 +0800

    Simplify localizePath() and delocalizePath(). (#33)

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -19,14 +19,11 @@ export const defaultLanguage = 'en';
  *   The target language.
  * @param currentLanguage
  *   The current language.
- * @param defaultLanguage
- *   The default language.
  */
 export const localizePath = (
   path: string,
   targetLanguage: string,
-  currentLanguage: string,
-  defaultLanguage: string
+  currentLanguage: string
 ) => {
   if (currentLanguage === targetLanguage) {
     return path;
@@ -34,25 +31,29 @@ export const localizePath = (
 
   const segments = path.split('/').slice(1);
 
-  if (currentLanguage === defaultLanguage) {
-    segments.splice(0, 0, targetLanguage);
-  } else if (targetLanguage === defaultLanguage) {
+  // Strip the current language from the URL.
+  if (currentLanguage !== defaultLanguage) {
     segments.splice(0, 1);
-  } else {
-    segments.splice(0, 1, targetLanguage);
   }
 
+  // Add the target language to the URL.
+  if (targetLanguage !== defaultLanguage) {
+    segments.unshift(targetLanguage);
+  }
+
+  // Remove any empty segments and then merge into a URL.
   return `/${segments.filter(s => s.length).join('/')}`;
 };
 
 /**
  * Remove a language prefix from the current path if present.
+ *
+ * @param path
+ *   The path to process.
+ * @param currentLanguage
+ *   The current language.
  */
-export const delocalizePath = (
-  path: string,
-  currentLanguage: string,
-  defaultLanguage: string
-) => {
+export const delocalizePath = (path: string, currentLanguage: string) => {
   if (currentLanguage === defaultLanguage) {
     return path;
   }
