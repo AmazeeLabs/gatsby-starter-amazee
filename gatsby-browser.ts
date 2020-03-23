78db8b219e25759ab435b5e4922248989c8d9d2b
JohnAlbin <virtually.johnalbin@gmail.com>
2020-03-23 14:43:13 +0800

    Convert gatsby-config.js, gatsby-browser.js, and gatsby-ssr.js to TypeScript.

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,5 +1,4 @@
 // Make sure the global stylesheet is added to the gatsby build.
 import '!style-loader!css-loader!postcss-loader!./src/styles/tailwind.css';
 
-// Export the central `wrapRootElement` function.
-export { wrapRootElement } from './src/utils/wrap-root-element';
+export { wrapRootElement } from './src/components/WrapRootElement';
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+// We register the TypeScript evaluator in gatsby-config so we don't need to do
+// it in any other .js file. It automatically reads TypeScript config from
+// tsconfig.json.
+require('ts-node').register();
 
-require('dotenv').config({ path: `.env` });
-
-// Read in additional environment variables based on the `CURRENT_APP_ENV`
-// environment variable.
-require('dotenv').config({
-  path: `.environments/${process.env.CURRENT_APP_ENV || 'local'}.env`,
-});
-
-module.exports = {
-  siteMetadata: {
-    // TODO: Adjust the static site navigation or remove it entirely.
-    navigation: [
-      { path: '/', label: 'Home' },
-      { path: '/films', label: 'Films' },
-      {
-        path: '/persons',
-        label: 'Characters',
-      },
-    ],
-  },
-  plugins: [
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-layout',
-    'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
-    // TODO: Update the data source configuration's typeName and fieldName.
-    {
-      resolve: 'gatsby-source-graphql',
-      options: {
-        typeName: 'swapi',
-        fieldName: 'swapi',
-        url: process.env.GATSBY_GRAPHQL_BUILD_ENDPOINT,
-      },
-    },
-  ],
-};
+// Use a TypeScript version of gatsby-config.js.
+module.exports = require('./gatsby-config.ts');
