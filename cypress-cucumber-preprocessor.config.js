1425b3b5940ba0025f852b12e1a926e1828220d5
JohnAlbin <virtually.johnalbin@gmail.com>
2020-06-04 22:45:46 +0800

    Update cypress-cucumber-preprocessor. #70

--- cypress-cucumber-preprocessor.config.js
+++ cypress-cucumber-preprocessor.config.js
@@ -1,7 +1,10 @@
+const nonGlobalStepBaseDir =
+  process.env.CYPRESS_TEST_LEVEL === 'api'
+    ? 'cypress/integration/api'
+    : 'cypress/integration/ui';
+
 module.exports = {
+  commonPath: `${nonGlobalStepBaseDir}/common`,
   nonGlobalStepDefinitions: true,
-  nonGlobalStepBaseDir:
-    process.env.CYPRESS_TEST_LEVEL === 'api'
-      ? 'cypress/integration/api/'
-      : 'cypress/integration/ui/',
+  stepDefinitions: nonGlobalStepBaseDir,
 };
