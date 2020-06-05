54ee8bf084d8acf1c896407aa460d0dcb9533e58
JohnAlbin <virtually.johnalbin@gmail.com>
2020-06-06 02:04:33 +0800

    Add cimode language to aid with t() development. Fixes #66

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,10 +1,19 @@
+// When i18next is using the "cimode" language, t() will return the
+// translation key. @see https://www.i18next.com/overview/api#changelanguage
+const devLangaugeCode = 'cimode';
+
 // TODO: Define the list of languages for this project.
 export const languageNames: { [key: string]: string } = {
   en: 'English',
   de: 'Deutsch',
+  [devLangaugeCode]: 'Developer',
 };
 
-export const languageCodes = Object.keys(languageNames);
+// TODO: Decide which build environments can see the fake "cimode" language.
+const displayDeveloperLanguage = true;
+export const languageCodes = Object.keys(languageNames).filter(
+  language => language !== devLangaugeCode || displayDeveloperLanguage
+);
 
 // TODO: Define the default language.
 export const defaultLanguage = 'en';
