ec782168456b6b6b95c90899bd5fa812d3ca0aac
JohnAlbin <JohnAlbin@users.noreply.github.com>
2020-06-10 06:32:22 +0000

    deploy: 29d5d9512a5b9d48742eee36547c94e2ff518373

--- component---src-pages-index-tsx-ff4193ad4a8cbd8d4eff.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{QeBL:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),c=a("9Koi"),l=a("M43v");t.default=Object(l.a)((function(){var e=Object(c.a)().t;return n.createElement(n.Fragment,null,n.createElement("h1",{className:"mb-8"},e("swapi.homepage.title","Welcome")),n.createElement("p",null,e("swapi.homepage.text","This is a Gatsby example project.")))}))}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-ff4193ad4a8cbd8d4eff.js.map
