720dd550bc92b7c8493e9ef43d4d5eb8e2e802bf
JohnAlbin <virtually.johnalbin@gmail.com>
2020-06-12 14:08:31 +0800

    Add OneColumn story. #86

---
+++ src/components/layouts/OneColumn/index.stories.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import OneColumn from './index';
+
+export default {
+  title: 'Components/Layouts/One Column',
+  component: OneColumn,
+};
+
+export const Default = () => (
+  <OneColumn>
+    <p>
+      This is content that has been placed in the `OneColumn` component. Every
+      child element of the component will be centered on the page. The
+      `OneColumn` component spans the entire width of the page, so styles like
+      background colors can be applied to it.
+    </p>
+  </OneColumn>
+);
