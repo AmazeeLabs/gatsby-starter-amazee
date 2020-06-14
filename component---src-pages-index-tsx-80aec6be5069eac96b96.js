5ff7a82fce2989535cb75d35c283a2cc05290c3d
JohnAlbin <JohnAlbin@users.noreply.github.com>
2020-06-14 10:32:22 +0000

    deploy: d2862b3d6a99c207a23721c137366e4037613d66

--- component---src-pages-index-tsx-80aec6be5069eac96b96.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{QeBL:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),c=a("9Koi"),i=a("CUij"),l=a("M43v");t.default=Object(l.a)((function(){var e=Object(c.a)().t;return n.createElement(i.a,null,n.createElement("h1",{className:"mb-8"},e("swapi.homepage.title","Welcome")),n.createElement("p",null,e("swapi.homepage.text","This is a Gatsby example project.")))}))}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-80aec6be5069eac96b96.js.map
---
+++ component---src-pages-index-tsx-b97d0bb2ed4e3ceba33d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{QeBL:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),s=a("9Koi"),c=a("CUij"),i=a("M43v");t.default=Object(i.a)((function(){var e=Object(s.a)().t;return n.createElement(c.a,null,n.createElement("h1",{className:"mb-8"},e("swapi-pages.home.title","Welcome")),n.createElement("p",null,e("swapi-pages.home.text","This is a Gatsby example project.")))}))}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-b97d0bb2ed4e3ceba33d.js.map
