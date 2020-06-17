2cb65ebe0e63eb618308e368150a648368aee17b
JohnAlbin <virtually.johnalbin@gmail.com>
2020-06-17 17:22:40 +0800

    Move containers components to src/components/containers. #102

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
 import { useTranslation } from 'react-i18next';
-import List from 'components/List';
+import List from 'components/containers/List';
 import Meta from 'components/common/Meta';
 import OneColumn from 'components/layouts/OneColumn';
 import Title from 'components/common/Title';
