5c21b62a70f8a9421b5536fa101470731a8977dc
JohnAlbin <virtually.johnalbin@gmail.com>
2020-06-18 23:06:59 +0800

    Remove duplicate code in templates/films-film. #107

--- src/templates/films-film.tsx
+++ src/templates/films-film.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { useTranslation } from 'react-i18next';
-import List from 'components/containers/List';
-import Meta from 'components/common/Meta';
-import OneColumn from 'components/layouts/OneColumn';
-import Title from 'components/common/Title';
+import FilmTemplate from 'components/pages/films/FilmTemplate';
 import withPageWrapper from 'hocs/withPageWrapper';
 
 // By exporting this query, we tell Gatsby to execute it with the context
@@ -40,35 +36,6 @@ const Page: React.FC<{
       };
     };
   };
-}> = ({ data }) => {
-  const { t } = useTranslation();
-  return (
-    <OneColumn>
-      <Meta
-        description={t('swapi.pages.films-film.description', {
-          title: data.swapi.film.title,
-        })}
-      />
-      <Title className="mb-2" sectionTitle={t('swapi.pages.films.title')}>
-        {data.swapi.film.title}
-      </Title>
-      {data.swapi.film.episodeId && (
-        <p className="mb-8">
-          {t('swapi.pages.films-film.episode', {
-            episodeId: data.swapi.film.episodeId,
-          })}
-        </p>
-      )}
-      <h2>{t('swapi.pages.films-film.characters')}</h2>
-      <List
-        items={data.swapi.film.characters.map((character) => ({
-          id: character.id,
-          label: character.name,
-          path: `/characters/${character.id}`,
-        }))}
-      />
-    </OneColumn>
-  );
-};
+}> = ({ data }) => <FilmTemplate film={data.swapi.film} />;
 
 export default withPageWrapper(Page);
