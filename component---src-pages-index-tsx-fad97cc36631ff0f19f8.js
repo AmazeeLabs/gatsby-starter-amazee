decf2e81a43ebd39cf39bf219f8e7b0d8274b677
JohnAlbin <JohnAlbin@users.noreply.github.com>
2020-06-21 06:32:00 +0000

    deploy: e98b040bf441da4fea54942e46cd6ee39b9d282d

--- component---src-pages-index-tsx-fad97cc36631ff0f19f8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{QeBL:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),i=t.n(n),p=t("9Koi"),s=t("333z"),c=t("CUij"),o=t("Rh6+"),r=function(){var e=Object(p.a)().t;return i.a.createElement(c.a,null,i.a.createElement(s.a,{description:e("swapi.pages.home.description")}),i.a.createElement(o.a,{isHomepage:!0},e("swapi.pages.home.title")),i.a.createElement("p",null,e("swapi.pages.home.text")))},l=t("M43v");a.default=Object(l.a)(r)}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-fad97cc36631ff0f19f8.js.map
