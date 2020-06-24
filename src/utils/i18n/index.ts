bb807079bd82a664a8649088321730c3d7850579
JohnAlbin <virtually.johnalbin@gmail.com>
2020-06-24 12:23:29 +0800

    Add code comments to i18n.

--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -1,3 +1,9 @@
+// This group of utilities is for a specific set of i18n features.
+//
+// To learn about the general concepts behind i18n and Gatsby, see:
+// https://www.gatsbyjs.org/blog/2020-02-19-how-to-build-multilingual-sites-with-gatsby/
+// https://github.com/gatsbyjs/gatsby-i18n-source/blob/master/docs/docs/localization-i18n.md
+
 import init from './init';
 import {
   delocalizePath,
