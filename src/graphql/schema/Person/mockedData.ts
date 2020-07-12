49a71d87323be2755811816f979fbe471119d2ed
JohnAlbin <virtually.johnalbin@gmail.com>
2020-07-12 19:49:56 +0800

    feat: sync mock data with swapi.graph.cool #23

--- src/graphql/schema/Person/mockedData.ts
+++ src/graphql/schema/Person/mockedData.ts
@@ -3,18 +3,23 @@ import { Person } from './index';
 // Films have circular references to Person, so they are created in the Film mocks.
 export const people: Person[] = [
   {
-    id: '1',
+    id: 'cj0nv9p8yewci0130wjy4o5fa',
     name: 'Luke Skywalker',
     films: [],
   },
   {
-    id: '2',
+    id: 'cj0nv9pakewcq01303eu3xuy1',
     name: 'Leia Organa',
     films: [],
   },
   {
-    id: '3',
+    id: 'cj0nv9pdlewd80130bs5vgryn',
     name: 'Han Solo',
+    films: [],
+  },
+  {
+    id: 'cj0nv9p9gewck0130h8f8esy0',
+    name: 'C-3PO',
     films: [],
   },
 ];
--- src/graphql/schema/mockedData.ts
+++ src/graphql/schema/mockedData.ts
@@ -2,13 +2,26 @@ import { films } from './Film/mockedData';
 import { people } from './Person/mockedData';
 
 // Persons and Films have circular references, so they are created here.
-people[0].films = [films[0], films[1]];
-people[1].films = [films[1], films[2]];
-people[2].films = [films[0], films[1], films[2]];
+people[0].films = [films[3], films[4]];
+people[1].films = [films[4], films[5]];
+people[2].films = [films[3], films[4], films[5]];
+people[3].films = [
+  films[0],
+  films[1],
+  films[2],
+  films[3],
+  films[4],
+  films[5],
+  films[6],
+];
 
-films[0].characters = [people[0], people[2]];
-films[1].characters = [people[0], people[1], people[2]];
-films[2].characters = [people[1], people[2]];
+films[0].characters = [people[3]];
+films[1].characters = [people[3]];
+films[2].characters = [people[3]];
+films[3].characters = [people[0], people[2], people[3]];
+films[4].characters = [people[0], people[1], people[2], people[3]];
+films[5].characters = [people[1], people[2], people[3]];
+films[6].characters = [people[3]];
 
 // Export the merged mocked data.
 export { films, people };
